9da88d9c9cfe138b2676bfc7a088c07ea34bd33d
Tiago <tiagoalex0202@gmail.com>
2023-02-12 07:48:17 -0300

    funções da estrutura do html

---
+++ src/Corpo.js
@@ -0,0 +1,14 @@
+import Stories from "./Stories";
+import Posts from "./Posts";
+
+export default function Corpo(){
+    return (
+      <div className="corpo">
+        <div className="esquerda">
+            <Stories/>
+            <Posts/>
+        </div>
+        
+      </div>
+    )
+}
---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import Sugestoes from "./Sugestoes"
+
+export default function Sidebar(){
+    return (
+        <div className="sidebar">
+            <div className="links">
+               Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+               Hashtags • Idioma
+            </div>
+            <div className="copyright">
+            © 2021 INSTAGRAM DO FACEBOOK
+            </div>
+        </div>
+    )
+}
